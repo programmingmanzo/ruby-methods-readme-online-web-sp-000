48f33d0443daa3f51ef3757e9fdc3a4c881fda13
Gerardo Manzo <programmingmanzo@gmail.com>
2020-03-19 01:59:19 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,15 +1,18 @@
 # Use this space to code along with the readme
 
 def say_hello_ten_times
-phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-puts phrase
-puts phrase
-puts phrase
+  phrase = "Hello World!"
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
 end
+
+say_hello_ten_times 
+
